eb92cffee8568d7a154851b014e477324ba5e1e1
Jan Chochol <jan.chochol@gooddata.com>
2022-12-18 13:15:26 +0100

    2022 Day 18

---
+++ 2022/18/p1.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+int main() {
+    std::string line;
+    const int max = 30;
+
+    bool pixels[max][max][max] = { false };
+
+    int max_x = 0, max_y = 0, max_z = 0;
+
+    while(std::getline(std::cin, line)) {
+        int comma1 = line.find(',');
+        int comma2 = line.find(',', comma1 + 1);
+
+        int x = stoi(line.substr(0, comma1));
+        int y = stoi(line.substr(comma1 + 1, comma2 - comma1 - 1));
+        int z = stoi(line.substr(comma2 + 1, line.size() - comma2 - 1));
+
+        if(x > max_x) {
+            max_x = x;
+        }
+        if(y > max_y) {
+            max_y = y;
+        }
+        if(z > max_z) {
+            max_z = z;
+        }
+
+        pixels[x][y][z] = true;
+    }
+
+    std::cout << max_x << " " << max_y << " " << max_z << " " << std::endl;
+
+    int sum = 0;
+
+    for (int x = 0; x < max; x++) {
+        for (int y = 0; y < max; y++) {
+            for (int z = 0; z < max; z++) {
+                if (pixels[x][y][z]) {
+                    if (x == 0) {
+                        sum ++;
+                    } else if (!pixels[x - 1][y][z]) {
+                        sum ++;
+                    }
+
+                    if (x == max - 1) {
+                        sum ++;
+                    } else if (!pixels[x + 1][y][z]) {
+                        sum ++;
+                    }
+
+                    if (y == 0) {
+                        sum ++;
+                    } else if (!pixels[x][y - 1][z]) {
+                        sum ++;
+                    }
+
+                    if (y == max - 1) {
+                        sum ++;
+                    } else if (!pixels[x][y + 1][z]) {
+                        sum ++;
+                    }
+
+                    if (z == 0) {
+                        sum ++;
+                    } else if (!pixels[x][y][z - 1]) {
+                        sum ++;
+                    }
+
+                    if (z == max - 1) {
+                        sum ++;
+                    } else if (!pixels[x][y][z + 1]) {
+                        sum ++;
+                    }
+                }
+            }
+        }
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
